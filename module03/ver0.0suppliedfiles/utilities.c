ff0149684ff18667cad69ee34e3325be0c3f699d
Steve Hussung <sthussun@iu.edu>
2019-02-10 15:04:05 -0500

    Moving over stuff from team2Dir

--- module03/ver0.0suppliedfiles/utilities.c
+++ module03/ver0.0suppliedfiles/utilities.c
@@ -1,4 +1,6 @@
+#define PARALLEL 1
 #include "utilities.h"
+#include <math.h>
 
 /* 09/12/02-10/10/02, updated 02/21/08 and 02/25/09 by Matthias K. Gobbert */
 
@@ -10,6 +12,8 @@ double serial_dot (double *x, double *y, long n)
   dp = 0.0;
 
   /* code needed here */
+  for (i = 0; i < n; i++)
+    dp = dp + x[i]*y[i];
 
   return dp;
 }
@@ -22,8 +26,16 @@ double parallel_dot(double *l_x, double *l_y, long l_n, MPI_Comm comm) {
   l_dot=serial_dot(l_x,l_y,l_n);
 
   /* code needed here */
+  MPI_Allreduce(&l_dot, &dot, 1, MPI_DOUBLE,
+                MPI_SUM, MPI_COMM_WORLD);
 
   return dot;
-
 }
 
+double parallel_norm2(double *l_x, long l_n, MPI_Comm comm) {
+  double norm2;
+
+  norm2=sqrt(parallel_dot(l_x,l_x,l_n,comm));
+
+  return norm2;
+}
