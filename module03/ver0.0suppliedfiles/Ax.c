241a80baa17b57d7fbc2f471b702e2d2f2fac524
Steve Hussung <sthussun@iu.edu>
2019-02-11 17:26:34 -0500

    Ax.c updated from Mengxi

--- module03/ver0.0suppliedfiles/Ax.c
+++ module03/ver0.0suppliedfiles/Ax.c
@@ -5,8 +5,7 @@ void Ax(double *l_v, double *l_u, long l_n, long l_N, long N,
         int id, int idleft, int idright, int np, MPI_Comm comm,
         double *gl, double *gr) {
 
-  /*long i, l_j;*/
-  long l_j;
+  long i, l_j;
   double tmp;
   MPI_Status statuses[4];
   MPI_Request requests[4];
