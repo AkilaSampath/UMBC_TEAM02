7b1a4806f7ef5434c0602a568f25104b76cae2f4
Mengxi <47191748+mengxi16@users.noreply.github.com>
2019-02-25 23:33:15 -0500

    Correct the matlab program

--- module05/hw5.m
+++ module05/hw5.m
@@ -1,84 +1,121 @@
-% inputs
 global N_tot
 N_tot=10000;
-tau_c=.1:.1:50;
-omega=.8;
-theta_0=pi/4;
+question=1;
 
-% variables
-dest=zeros(N_tot,length(tau_c),length(omega));
-tau_star=dest;
-R=zeros(length(tau_c),length(omega));
+if(question==1)
+    tau_c=.1:.1:20;
+    omega=[1,.9,.8];
+    theta_0=0;
+end
+if(question==2)
+    tau_c=10;
+    omega=[1,.96,.92,.88];
+    theta_0=0;
+end
+if(question==3)
+    tau_c=.1:.1:50;
+    omega=.8;
+    theta_0=pi/4;
+end
+
+R=zeros(length(omega),length(tau_c));
 A=R;
 T=R;
+tau_star=zeros(N_tot,length(omega),length(tau_c));
 
-for i=1:length(tau_c)
-    for j=1:length(omega)
-        [N_ref,N_abs,N_tra,dest(:,i,j),tau_star(:,i,j)]=computation(tau_c(i),omega(j),theta_0);
+for i=1:length(omega)
+    for j=1:length(tau_c)
+        [N_ref,N_abs,N_tra,tau_star(:,i,j)]=computation(tau_c(j),omega(i),theta_0);
         R(i,j)=N_ref/N_tot;
         A(i,j)=N_abs/N_tot;
         T(i,j)=N_tra/N_tot;
     end
 end
 
-% hold on
-% plot(tau_c,R(:,1),'LineWidth',2)
-% plot(tau_c,R(:,2),'LineWidth',2)
-% plot(tau_c,R(:,3),'LineWidth',2)
-% hold off
-% legend('\omega=1.0','\omega=0.9','\omega=0.8','Location','northwest')
-% ax=gca;
-% ax.FontSize=20;
+if(question==1)
+    hold on
+    for i=1:length(omega)
+        plot(tau_c,R(i,:),'LineWidth',2)
+    end
+    hold off
+    legend('\omega=1.0','\omega=0.9','\omega=0.8','Location','northwest')
+    ax=gca;
+    ax.FontSize=20;
+end
 
-% hold on
-% histogram(tau_star(dest(:,1,1)==1,1,1))
-% histogram(tau_star(dest(:,1,2)==1,1,2))
-% histogram(tau_star(dest(:,1,3)==1,1,3))
-% histogram(tau_star(dest(:,1,4)==1,1,4))
-% hold off
-% legend('\omega=1.00','\omega=0.96','\omega=0.92','\omega=0.88','Location','northwest')
-% ax=gca;
-% ax.FontSize=20;
+if(question==2)
+    hold on
+    for j=1:length(omega)
+        histogram(tau_star(:,j),0:.5:10,'Normalization','probability')
+    end
+    hold off
+    legend('\omega=1.00','\omega=0.96','\omega=0.92','\omega=0.88','Location','northeast')
+    ax=gca;
+    ax.FontSize=20;
+end
 
-P=gammadist(tau_c');
-mean_R=.1*(.5*(P(1)*R(1)+P(end)*R(end))+sum(P(2:end-1).*R(2:end-1)));
+if(question==3)
+    P=gammadist(tau_c);
+    mean_R=.1*(.5*(P(1)*R(1)+P(end)*R(end))+sum(P(2:end-1).*R(2:end-1)));
+end
 
-function [N_ref,N_abs,N_tra,dest,tau_star]=computation(tau_c,omega,theta_0)
+function [N_ref,N_abs,N_tra,tau_star]=computation(tau_c,omega,theta_0)
+global N_tot
+tau_star=zeros(N_tot,1);
+N_ref=0;
+N_abs=0;
+N_tra=0;
 
-[dest,tau_star]=photon(tau_c,omega,theta_0);
-
-% output
+y=tau_star;
+ctheta=ones(N_tot,1)*cos(theta_0);
+dest=y;
+id=(1:N_tot)';
+ld=N_tot;
+while(ld)
+    rng('shuffle')
+    tau=-log(rand(ld,1));
+    y(id)=y(id)+tau.*ctheta;
+    dest(id(y(id)<=0))=1;
+    dest(id(y(id)>=tau_c))=3;
+    id=id(dest(id)==0);
+    ld=length(id);
+    isabsorbed=(rand(ld,1)>omega);
+    dest(id(isabsorbed))=2;
+    id=id(isabsorbed==0);
+    ld=length(id);
+    ctheta=1-2*rand(ld,1);
+    tau_star(id)=max(tau_star(id),y(id));
+end
+tau_star(dest>1)=NaN;
 N_ref=sum(dest==1);
 N_abs=sum(dest==2);
 N_tra=sum(dest==3);
-end
 
-function [dest,tau_star]=photon(tau_c,omega,theta_0)
-global N_tot
-y=zeros(N_tot,1);
-dest=y;
-tau_star=y;
-isabsorbed=y;
-theta=repmat(theta_0,[N_tot,1]);
-id=ones(N_tot,1);
-ld=N_tot;
-
-while(ld)
-    f=rand(ld,1);
-    tau=-log(f);
-    y(id)=y(id)+tau.*cos(theta);
-    tau_star(id)=max([tau_star(id);y(id)]);
-    dest(id & y<0)=1;
-    dest(id & y>=tau_c)=3;
-    id=(dest==0);
-    ld=sum(id);
-    isabsorbed(id)=(rand(ld,1)>omega);
-    dest(id & isabsorbed)=2;
-    id=(dest==0);
-    ld=sum(id);
-    theta=rand(ld,1)*pi;
-end
-tau_star(tau_star>tau_c)=tau_c;
+% for i=1:N_tot
+%     y=0;
+%     ctheta=cos(theta_0);
+%     rng('shuffle')
+%     while(1)
+%         tau=-log(rand());
+%         y=y+tau*ctheta;
+%         if(y<=0)
+%             N_ref=N_ref+1;
+%             break
+%         end
+%         if(y>=tau_c)
+%             N_tra=N_tra+1;
+%             tau_star(i)=NaN;
+%             break
+%         end
+%         if(rand()>omega)
+%             N_abs=N_abs+1;
+%             tau_star(i)=NaN;
+%             break
+%         end
+%         ctheta=1-2*rand();
+%         tau_star(i)=max(tau_star(i),y);
+%     end
+% end
 end
 
 function P=gammadist(tau)
